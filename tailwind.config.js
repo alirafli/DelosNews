decf417991700efbf6e95424227eb782e42d436b
alirafli <mrafli1212@gmail.com>
2023-03-21 17:01:01 +0700

    add: color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,19 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1C9AD0",
+        "primary-dark": "#21334D",
+        "primary-sky": "#2F75BA",
+        black: "#222222",
+        grey: "#3A3A3C",
+        "brown-peanut": "#84754D",
+      },
+    },
   },
   plugins: [],
 };
