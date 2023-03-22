c64f38d87e8bceb9fe8c7bc6b6f866f93e0ca394
alirafli <mrafli1212@gmail.com>
2023-03-23 02:00:16 +0700

    add: jumbotron

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,50 @@
 import type { NextPage } from "next";
-import { Button, Meta, Navbar, Text } from "@components/elements";
+import { Button, Meta, Text, SimpleArticleCard } from "@components/elements";
+import { TopFour } from "@components/modules";
+import JUMBOTRON from "@assets/images/exciting-news-amico.svg";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="p-28 min-h-screen">
+    <div className="py-28 px-10 xl:px-20 min-h-screen">
       <Meta subTitle="Homepage" />
-      Hello World!
-      <Button>Sign Up</Button>
-      <Button variant="secondary">Sign In</Button>
-      <Text>Kelamaan nunggu?</Text>
-      <Text variant="jumboSubTitle">
-        Pesan meja dan makanan lebih cepat dan praktis
-      </Text>
-      <Text variant="title">
-        Koleksi di <span className="font-bold">Jakarta</span>
-      </Text>
-      <Text variant="subTitle">
-        Temukan restoran, bar, cafe terbaik di lokasimu
-      </Text>
+
+      <div className="flex flex-col-reverse md:flex-row items-center justify-between mb-40">
+        <Text
+          variant="jumboTitle"
+          className="md:w-1/2 text-center md:text-left"
+        >
+          Get <span className="text-primary">the Best</span> of The New York
+          Times Article with DelosNews: Your One-Stop Shop!
+        </Text>
+        <Image
+          alt="jumbotron"
+          src={JUMBOTRON}
+          width={350}
+          className="px-5 md:px-0 mb-7 md:mb-0 "
+        />
+      </div>
+
+      <TopFour title="Most Emailed">
+        <SimpleArticleCard />
+        <SimpleArticleCard />
+        <SimpleArticleCard />
+        <SimpleArticleCard />
+      </TopFour>
+
+      <TopFour title="Most Shared">
+        <SimpleArticleCard />
+        <SimpleArticleCard />
+        <SimpleArticleCard />
+        <SimpleArticleCard />
+      </TopFour>
+
+      <TopFour title="Most Viewed">
+        <SimpleArticleCard />
+        <SimpleArticleCard />
+        <SimpleArticleCard />
+        <SimpleArticleCard />
+      </TopFour>
     </div>
   );
 };
