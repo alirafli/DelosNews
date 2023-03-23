f15e7d02dfddeaab49c5c00424650f09551fb20c
alirafli <mrafli1212@gmail.com>
2023-03-24 02:25:50 +0700

    revamp: simple article card data flow

--- components/elements/Card/SimpleArticleCard/index.tsx
+++ components/elements/Card/SimpleArticleCard/index.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { FC } from "react";
 import { Text } from "@components/elements";
 import styles from "./SimpleCard.module.css";
 import Link from "next/link";
 
-export const SimpleArticleCard = () => {
+type SimpleCardprops = {
+  title: string;
+  writer: string;
+  background: string;
+  id: number;
+  link: string;
+};
+
+export const SimpleArticleCard: FC<SimpleCardprops> = ({
+  title = "...",
+  writer = "...",
+  background,
+  id,
+  link,
+}) => {
   return (
-    <Link href="" className={`${styles.container} bg-default-card bg-cover`}>
-      <div className={styles.textWrap}>
-        <Text variant="subTitle">Author</Text>
-        <Text>title</Text>
+    <Link
+      href=""
+      className={`${styles.container} bg-cover`}
+      style={{
+        backgroundImage: `url("${background}")`,
+      }}
+    >
+      <div className={`${styles.textWrap} truncate`}>
+        <Text variant="subTitle" className="truncate">{writer}</Text>
+        <Text className="truncate">{title}</Text>
       </div>
     </Link>
   );
