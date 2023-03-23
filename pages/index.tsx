86001d15cb93386e4d74874f17459c1b1659e114
alirafli <mrafli1212@gmail.com>
2023-03-24 02:37:03 +0700

    feat: implement article fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,44 @@
 import type { NextPage } from "next";
-import { Button, Meta, Text, SimpleArticleCard } from "@components/elements";
+import { Meta, Text, SimpleArticleCard } from "@components/elements";
 import { TopFour } from "@components/modules";
 import JUMBOTRON from "@assets/images/exciting-news-amico.svg";
 import Image from "next/image";
+import {
+  getArticlesEmailed,
+  getArticlesShared,
+  getArticlesViewed,
+} from "fetches/article";
+import { useEffect, useState } from "react";
+import { ArticleData } from "@types";
 
 const Home: NextPage = () => {
+  const [emailedArticle, setEmailedArticle] = useState<ArticleData[]>([]);
+  const [sharedArticle, setSharedArticle] = useState<ArticleData[]>([]);
+  const [viewedArticle, setViewedArticle] = useState<ArticleData[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchArticles = async () => {
+    try {
+      const ArticlesEmailed = await getArticlesEmailed(7);
+      setEmailedArticle(ArticlesEmailed.data.results);
+
+      const ArticlesShared = await getArticlesShared(7);
+      setSharedArticle(ArticlesShared.data.results);
+
+      const ArticlesViewe = await getArticlesViewed(7);
+      setViewedArticle(ArticlesViewe.data.results);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchArticles();
+  }, []);
+
   return (
     <div className="py-28 px-10 xl:px-20 min-h-screen">
       <Meta subTitle="Homepage" />
@@ -24,27 +58,56 @@ const Home: NextPage = () => {
           className="px-5 md:px-0 mb-7 md:mb-0 "
         />
       </div>
+      {isLoading ? (
+        <Text>loading...</Text>
+      ) : (
+        <>
+          <TopFour title="Most Emailed">
+            {emailedArticle.slice(0, 4).map((data, key) => {
+              return (
+                <SimpleArticleCard
+                  key={key}
+                  id={data.id}
+                  link={data.url}
+                  title={data.title}
+                  writer={data.byline}
+                  background={data?.media[0]["media-metadata"][2]?.url}
+                />
+              );
+            })}
+          </TopFour>
 
-      <TopFour title="Most Emailed">
-        <SimpleArticleCard />
-        <SimpleArticleCard />
-        <SimpleArticleCard />
-        <SimpleArticleCard />
-      </TopFour>
+          <TopFour title="Most Shared">
+            {sharedArticle.slice(0, 4).map((data, key) => {
+              return (
+                <SimpleArticleCard
+                  key={key}
+                  id={data.id}
+                  link={data.url}
+                  title={data.title}
+                  writer={data.byline}
+                  background={data?.media[0]["media-metadata"][2]?.url}
+                />
+              );
+            })}
+          </TopFour>
 
-      <TopFour title="Most Shared">
-        <SimpleArticleCard />
-        <SimpleArticleCard />
-        <SimpleArticleCard />
-        <SimpleArticleCard />
-      </TopFour>
-
-      <TopFour title="Most Viewed">
-        <SimpleArticleCard />
-        <SimpleArticleCard />
-        <SimpleArticleCard />
-        <SimpleArticleCard />
-      </TopFour>
+          <TopFour title="Most Viewed">
+            {viewedArticle.slice(0, 4).map((data, key) => {
+              return (
+                <SimpleArticleCard
+                  key={key}
+                  id={data.id}
+                  link={data.url}
+                  title={data.title}
+                  writer={data.byline}
+                  background={data?.media[0]["media-metadata"][2]?.url}
+                />
+              );
+            })}
+          </TopFour>
+        </>
+      )}
     </div>
   );
 };
